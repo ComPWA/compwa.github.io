5bbb14183ce163e64a940f2cbb5bc1e09f51885c
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-02-06 17:40:18 +0100

    fix: add pwa to intersphinx inventory

--- docs/conf.py
+++ docs/conf.py
@@ -223,6 +223,7 @@ intersphinx_mapping = {
         None,
     ),
     "numpy": (f"https://numpy.org/doc/{get_minor_version('numpy')}", None),
+    "pwa": ("https://pwa.readthedocs.io", None),
     "python": ("https://docs.python.org/3", None),
     "qrules": ("https://qrules.readthedocs.io/en/stable", None),
     "scipy": ("https://docs.scipy.org/doc/scipy-1.7.0", None),
