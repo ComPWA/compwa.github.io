28239db34585a947f12155a4e830d2a9c856c5c1
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-08 21:36:39 +0100

    FIX: relink `ipywidgets` intersphinx inventory

--- docs/conf.py
+++ docs/conf.py
@@ -167,6 +167,10 @@ nitpicky = True  # warn if cross-references are missing
 
 # Intersphinx settings
 version_remapping = {
+    "ipywidgets": {
+        "8.0.3": "8.0.2",
+        "8.0.4": "8.0.2",
+    },
     "matplotlib": {"3.5.1": "3.5.0"},
 }
 
